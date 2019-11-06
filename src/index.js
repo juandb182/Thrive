2bf80bc63c8125d7dae1c62006f022bdad567c86
juan duran <juandb182@gmail.com>
2019-11-06 14:16:40 -0400

    fix error

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const multer = require('multer');
+const uuid = require('uuid/v4');
 
 //initialization
 const app = express();
@@ -18,7 +19,14 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 //understand the inputs in my server
 app.use(express.urlencoded({extended:false}));
-app.use(multer({dest: path.join(__dirname, 'public/img/uploads')}).single('image'));
+//to put the type of the file
+const storage = multer.diskStorage({
+    destination : path.join(__dirname, 'public/img/uploads'), 
+    filename:(req,file,cb,filename) => {
+        cb(null,uuid() + path.extname(file.originalname)); 
+    }
+});
+app.use(multer({storage: storage}).single('image'));
 //global variables
 
 //ROUTES
