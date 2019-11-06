c704058e8f6dca70307a11d783a492cad2297d08
juan duran <juandb182@gmail.com>
2019-11-05 22:02:08 -0400

    More routes and fix routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,3 +2,22 @@ const {Router} = require('express');
 const router = Router();
 
 module.exports = router;
+
+router.get('/',(req, res) =>{
+    res.send('Index page');
+});
+
+router.get('/upload' ,(req, res) =>{
+    res.send('Form');
+});
+
+router.post('/upload',(req, res)=>{
+    res.send('uploaded');
+});
+
+router.get('/image/:id',(req, res) => {
+    res.send('profile');
+});
+router.get('/image/:id/delete',(req, res) => {
+    res.send('img deleted');
+});
